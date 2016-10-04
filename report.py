1219a6bed973a4ac3a6266c76954d587af03c5af
Bohdan Shepelinski <notfier@gmail.com>
2016-10-04 23:38:46 +0300

    refactoring, gitignore

--- report.py
+++ report.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import datetime
 import urllib.request
 import json
@@ -10,65 +11,96 @@ url = 'https://api.github.com/repos/{0}/{1}/commits'
 branches = 'https://api.github.com/repos/{0}/{1}/branches'
 
 
-def insert_params(start_date, last_date, branch):
-    return '?since={0}T00:00:0200&until={1}T23:59:0200&sha={2}'.format(
-        start_date, last_date, branch
-    )
+class Info():
 
-def getCommits(params, username, password, author_name):
-    """
-    Get raw Github commits data.
-    """
-    # get specific user commits or all the commits if the author is None
-    if author_name:
-        author = '&author={0}'.format(author_name)
-    req = urllib.request.Request(url.format(repo_owner, repo_name) + params + author)
-    string = '%s:%s' % (username, password)
-    bebo = string.encode('utf-8')
+    def __init__(self, *args, **kwargs):
+        self.repo_name = kwargs.get('repo_name')
+        self.repo_owner = kwargs.get('repo_owner')
+        self.start_date = kwargs.get('start_date')
+        self.last_date = kwargs.get('last_date')
+        self.branch = kwargs.get('branch')
+        self.author_name = kwargs.get('authore_name')
+        self.username = kwargs.get('username')
+        self.password = kwargs.get('password')
 
-    req.add_header('Authorization', 'Basic ' + base64.urlsafe_b64encode(bebo).decode('utf-8'))
-    req.add_header('Content-Type', 'application/json')
-    req.add_header('Accept', 'application/json')
-    resp = urllib.request.urlopen(req)
-
-    parsed_data = json.loads(resp.read().decode('utf-8'))
-
-    return parsed_data
-
-def createReport(commits):
-    """
-    Parse and create string Report.
-    """
-    report = ''
-    count = 1
-    date = None
-    for commit in commits:
-        raw_date = commit.get('commit').get('author').get('date')
-        converted_date = datetime.datetime.strptime(
-            raw_date[:-1], "%Y-%m-%dT%H:%M:%S"
+    def insert_params(self):
+        return '?since={0}T00:00:0200&until={1}T23:59:0200&sha={2}'.format(
+            self.start_date, self.last_date, self.branch
         )
-        if not date:
-            date = converted_date.strftime('%Y-%m-%d')
-            report += '{0}\n'.format(date)
-        if date != converted_date.strftime('%Y-%m-%d'):
-            date = converted_date.strftime('%Y-%m-%d')
-            report += '\n{0}\n'.format(date)
-            count = 1
-        report += '{0}) description: {1}\n'.format(
-            count,
-            commit.get('commit').get('message')
-        )
-        report += 'url: {0}\n'.format(commit.get('html_url'))
-        count += 1
-    return report
 
-def writeReport(report, start_date, last_date):
-    """
-    Write commits information to the file.
-    """
-    f = open('report({0}|{1}).txt'.format(start_date, last_date), 'w')
-    f.write(report)
-    f.close()
+    def getCommits(self):
+        """
+        Get raw Github commits data.
+        """
+        params = self.insert_params()
+        # get specific user commits or all the commits if the author is None
+        if self.author_name:
+            full_url = url.format(
+                self.repo_owner,
+                self.repo_name
+            ) + params + '&author={0}'.format(
+                self.author_name
+            )
+        else:
+            full_url = url.format(self.repo_owner, self.repo_name) + params
+
+        req = urllib.request.Request(full_url)
+        string = '%s:%s' % (self.username, self.password)
+        bebo = string.encode('utf-8')
+
+        req.add_header('Authorization', 'Basic ' + base64.urlsafe_b64encode(bebo).decode('utf-8'))
+        req.add_header('Content-Type', 'application/json')
+        req.add_header('Accept', 'application/json')
+        resp = urllib.request.urlopen(req)
+
+        parsed_data = json.loads(resp.read().decode('utf-8'))
+
+        return parsed_data
+
+    def createReport(self):
+        """
+        Parse and create string Report.
+        """
+        report = ''
+        count = 1
+        date = None
+        commits = self.getCommits()
+        for commit in commits:
+            raw_date = commit.get('commit').get('author').get('date')
+            converted_date = datetime.datetime.strptime(
+                raw_date[:-1], "%Y-%m-%dT%H:%M:%S"
+            )
+            if not date:
+                date = converted_date.strftime('%Y-%m-%d')
+                report += '{0}\n'.format(date)
+            if date != converted_date.strftime('%Y-%m-%d'):
+                date = converted_date.strftime('%Y-%m-%d')
+                report += '\n{0}\n'.format(date)
+                count = 1
+            report += '{0}) description: {1}\n'.format(
+                count,
+                commit.get('commit').get('message')
+            )
+            report += 'url: {0}\n'.format(commit.get('html_url'))
+            count += 1
+        return report
+
+    def writeReport(self):
+        """
+        Write commits information to the file.
+        """
+        report = self.createReport()
+
+        folder_path = os.path.join(os.getcwd(), 'reports')
+        if not os.path.exists(folder_path):
+            os.mkdir(folder_path)
+        file_name = os.path.join(folder_path, 'report({0}|{1}).txt'.format(
+            self.start_date,
+            self.last_date
+        ))
+        with open(file_name, 'w') as file:
+            file.write(report)
+
 
 def run_script():
     """
@@ -106,10 +138,17 @@ def run_script():
         raise Exception('You didn\'t enter password!')
 
     # run report creating
-    params = insert_params(start_date, last_date, branch)
-    commits = getCommits(params, username, password, author_name)
-    report = createReport(commits)
-    writeReport(report, start_date, last_date)
+    info = Info(
+        repo_owner=repo_owner,
+        repo_name=repo_name,
+        start_date=start_date,
+        last_date=last_date,
+        branch=branch,
+        author_name=author_name,
+        username=username,
+        password=password
+    )
+    info.writeReport()  # create file with commits
 
 
 if __name__ == '__main__':
